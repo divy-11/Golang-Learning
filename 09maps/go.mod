3b1ff921265ae22e135f45b52255b0bc919c95a6
divy-11 <149963660+divy-11@users.noreply.github.com>
2025-03-04 11:56:26 +0530

    Day3.1

---
+++ 09maps/go.mod
@@ -0,0 +1,3 @@
+module mapsORhashtable
+
+go 1.23.1
---
+++ 09maps/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	//maps -> hash Table -> key value pair
+
+	languages := make(map[string]string)
+	languages["JS"] = "Javascript"
+	languages["PY"] = "Python"
+	languages["TS"] = "Typescript"
+	languages["RB"] = "Ruby"
+	fmt.Println("Shwoing :", languages)
+
+	delete(languages, "RB")
+	fmt.Println("After deleting RB :", languages)
+
+	for key, val := range languages {  //we can use _,val if we dont wanna print key
+		fmt.Printf("The key is %v and the value is %v \n", key, val)
+	}
+}
