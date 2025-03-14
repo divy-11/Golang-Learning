41ee234884377de16fe440f133758a95fb9b56f4
divy-11 <devvv.sangwan@gmail.com>
2025-03-15 00:07:45 +0530

    Day8.1
    Didn't go well.

---
+++ 18webreq/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	const url = "https://lco.dev"
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Response is of type: %T\n", resp)
+	defer resp.Body.Close() //its our resp to close the connection in the end.
+	contentBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(contentBytes))
+}
