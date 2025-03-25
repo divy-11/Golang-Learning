0775ca3e8113bec7b59ce0a37cd4385eaa7bed01
divy-11 <devvv.sangwan@gmail.com>
2025-03-25 21:26:56 +0530

    Day 13

--- 25mongoAPI/controller/controller.go
+++ 25mongoAPI/controller/controller.go
@@ -6,6 +6,8 @@ import (
 	"log"
 
 	"github.com/divy-11/mongoapi/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -36,4 +38,52 @@ func addOneMovie(movie model.Netflix) {
 		log.Fatal(err)
 	}
 	fmt.Println(ins)
+}
+
+func setMovieWatched(movieID string) {
+	id, _ := primitive.ObjectIDFromHex(movieID)
+	filter := bson.M{"_id": id}
+	updateInfo := bson.M{"$set": bson.M{"watched": true}}
+
+	res, err := collection.UpdateOne(context.Background(), filter, updateInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Count:", res.ModifiedCount)
+}
+
+func delOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	res, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res.DeletedCount)
+}
+
+func deleteAll() int64 {
+	res, err := collection.DeleteMany(context.Background(), bson.M{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return res.DeletedCount
+}
+
+func allMovies() []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.M{{}})
+	defer cur.Close(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	var movies []primitive.M
+	for cur.Next(context.Background()) {
+		var movie bson.M
+		err := cur.Decode(&movie)
+		if err != nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+	}
+	return movies
 }
